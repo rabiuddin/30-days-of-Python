1389f08eebafb87e6718431a5ab4972ab3c0ccaa
Nofal <Nofal@DESKTOP-U4KK52T>
2024-01-22 00:13:26 +0500

    Completed Task 2 of Day 1

---
+++ Day_1/Task_2.py
@@ -0,0 +1,19 @@
+a = 20
+print(type(a))
+a = 0.98
+print(type(a))
+a = 1 + 15j
+print(type(a))
+a = "Rabi Uddin"
+print(type(a))
+a = [1, "Rabi", 6.4]
+print(type(a))
+a = {
+    'Name' : "Rabi Uddin",
+    'Roll No' : "79708" 
+     }
+print(type(a))
+a = {2, 67, 45}
+print(type(a))
+a = (45, 76, 34)
+print(type(a))
