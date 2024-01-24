b01f4ae1dde2df0ef9f548ce06f686010a730324
Nofal <Nofal@DESKTOP-U4KK52T>
2024-01-24 22:26:18 +0500

    Task 3 of day 1 completed

---
+++ Day_1/Task_3.py
@@ -0,0 +1,8 @@
+first_name = input("Enter your first name: ")
+second_name = input("Enter your second name: ")
+country = input("Enter you country: ")
+statement = "I am enjoying 30 days of Python."
+
+print(f"My name is {first_name} {second_name}.")
+print(f"I live in {country}.")
+print(statement)
