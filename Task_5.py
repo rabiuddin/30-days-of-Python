4172c731a86b0cd399e97a2132c3c92b1dbf9280
Nofal <Nofal@DESKTOP-U4KK52T>
2024-01-25 23:17:13 +0500

    Task 5: Euclidean Distance

---
+++ Task_5.py
@@ -0,0 +1,7 @@
+import math
+#Euclidean Distance between (2,3) and (10,8)
+# (x2 - x1)
+x_diff = 10 - 2
+# (y2 - y1)
+y_diff = 8 - 3
+print(f"Euclidean Diatance: {math.sqrt((x_diff ** 2) + (y_diff ** 2))}")
