54b83f912492b248da8b6947576634dde56bbafc
Rabi Uddin <rabiuddin1@gmail.com>
2024-02-02 21:12:50 +0500

    Task 1 of Day 2 completed

---
+++ Day_2/Task_1.py
@@ -0,0 +1,12 @@
+#Day2: 30 Days of python programming
+first_name = "Muhammad"
+last_name = "Rabi Uddin"
+full_name = first_name + last_name
+country = "Pakistan"
+city = "Lahore"
+age = 21
+year = 2024
+is_married = False
+is_true = True
+is_light_on = False
+x = y = z = 4
