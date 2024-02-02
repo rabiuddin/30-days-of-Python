76f6d81772004b77b7cb8ea7769492481a7da87d
Rabi Uddin <rabiuddin1@gmail.com>
2024-02-02 21:19:48 +0500

    Task 3 of day 2 completed

---
+++ Day_2/Task_3.py
@@ -0,0 +1,8 @@
+radius = int(input("Enter Radius of circle: "))
+area_of_circle = 3.14 * radius * radius
+circum_of_circle = 2 * 3.14 * radius
+
+first_name = input("Enter your first name: ")
+last_name = input("ENter your last name: ")
+country = input("Enter your country: ")
+age = int(input("Enter your age: "))
