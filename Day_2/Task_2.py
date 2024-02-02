8c568cbcfe8d932183cd199f8d9a83f0b71da5cd
Rabi Uddin <rabiuddin1@gmail.com>
2024-02-02 21:13:08 +0500

    Task 2 of day 2 completed

---
+++ Day_2/Task_2.py
@@ -0,0 +1,31 @@
+#Day2: 30 Days of python programming
+first_name = "Muhammad"
+last_name = "Rabi Uddin"
+full_name = first_name + last_name
+country = "Pakistan"
+city = "Lahore"
+age = 21
+year = 2024
+is_married = False
+is_true = True
+is_light_on = False
+x = y = z = 4
+
+print(type(first_name))
+print(type(full_name))
+print(type(age))
+print(type(is_light_on))
+print(type(y))
+
+print(f"Length of first name: {len(first_name)}" )
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one/num_two
+remainder = num_two % num_one
+exp = num_one**num_two
+floor_division = num_one//num_two
