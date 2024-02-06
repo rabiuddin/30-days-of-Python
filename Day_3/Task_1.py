2bcb05c676b627e1b4e8ed3e980c9f7dc4dd5573
Rabi Uddin <rabiuddin1@gmail.com>
2024-02-06 11:12:08 +0500

    Task 1 of day 3 complete

---
+++ Day_3/Task_1.py
@@ -0,0 +1,23 @@
+# Task 1.1
+age = 34
+
+# Task 1.2
+height = 6.0
+
+# Task 1.3
+complex_no = 3 + 2j
+
+# Task 1.4
+base_of_triangle = int(input("Enter base of the triangle: "))
+height_of_triangle = int(input("Enter height of the triangle: "))
+area_of_triangle = 0.5 * base_of_triangle * height_of_triangle
+
+print(f"\nThe area of the triangle is {area_of_triangle}\n")
+
+# Task 1.5
+side_a = int(input("Enter side 'a' of triangle: "))
+side_b = int(input("Enter side 'b' of triangle: "))
+side_c = int(input("Enter side 'c' of triangle: "))
+perimeter = side_a + side_b + side_c
+
+print(f"\nThe perimeter of the triangle is: {perimeter}")
